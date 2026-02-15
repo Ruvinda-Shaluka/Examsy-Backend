1fbd8f3a951862fdacb2bb50eca4b18c5fc69fd3
Ruvinda24 <shalukaofficial24@gmail.com>
2026-02-15 12:26:55 +0530

    initialize Examsy Backend project with Spring Boot and Maven setup

---
+++ src/main/java/lk/ijse/examsybackend/ExamsyBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.examsybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamsyBackendApplication.class, args);
+    }
+
+}
