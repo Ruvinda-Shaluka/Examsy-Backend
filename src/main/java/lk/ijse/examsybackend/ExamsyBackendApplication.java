441b6410d6b0f92a88ff5aaba49293d3c717573e
Ruvinda24 <shalukaofficial24@gmail.com>
2026-02-27 11:01:23 +0530

    feat: configuring ModelMapper in ExamsyBackendApplication with @Bean

--- src/main/java/lk/ijse/examsybackend/ExamsyBackendApplication.java
+++ src/main/java/lk/ijse/examsybackend/ExamsyBackendApplication.java
@@ -1,13 +1,24 @@
 package lk.ijse.examsybackend;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExamsyBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExamsyBackendApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 
 }
