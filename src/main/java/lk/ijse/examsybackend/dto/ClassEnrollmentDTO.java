b43f8e76977c0cbb61c35fc60e9e13b3a9d93029
Ruvinda24 <shalukaofficial24@gmail.com>
2026-02-27 15:54:27 +0530

    feat: validated ClassEnrollmentDTO

--- src/main/java/lk/ijse/examsybackend/dto/ClassEnrollmentDTO.java
+++ src/main/java/lk/ijse/examsybackend/dto/ClassEnrollmentDTO.java
@@ -1,4 +1,30 @@
 package lk.ijse.examsybackend.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ClassEnrollmentDTO {
+
+    @Positive(message = "ID must be a valid positive integer")
+    private Integer id;
+
+    // Enforces the relationship to the course
+    @NotNull(message = "Course ID is required")
+    private Integer courseId;
+
+    // Enforces the relationship to the student
+    @NotNull(message = "Student ID is required")
+    private Integer studentId;
+
+    private LocalDateTime enrolledAt;
 }
