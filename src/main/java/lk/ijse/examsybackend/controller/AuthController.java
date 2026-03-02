b9b824253eeec06b44e78d04c9a6d9a9b518f34b
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-02 22:13:42 +0530

    feat: add AuthController for user registration and authentication endpoints

---
+++ src/main/java/lk/ijse/examsybackend/controller/AuthController.java
@@ -0,0 +1,35 @@
+package lk.ijse.examsybackend.controller;
+
+import jakarta.validation.Valid;
+import lk.ijse.examsybackend.dto.APIResponse;
+import lk.ijse.examsybackend.dto.AuthDTO;
+import lk.ijse.examsybackend.dto.StudentRegisterDTO;
+import lk.ijse.examsybackend.dto.TeacherRegisterDTO;
+import lk.ijse.examsybackend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("signup/student")
+    public ResponseEntity<APIResponse> registerStudent(@Valid @RequestBody StudentRegisterDTO dto) {
+        return ResponseEntity.ok(new APIResponse(200, "OK", authService.registerStudent(dto)));
+    }
+
+    @PostMapping("signup/teacher")
+    public ResponseEntity<APIResponse> registerTeacher(@Valid @RequestBody TeacherRegisterDTO dto) {
+        return ResponseEntity.ok(new APIResponse(200, "OK", authService.registerTeacher(dto)));
+    }
+
+    @PostMapping("signin")
+    public ResponseEntity<APIResponse> loginUser(@Valid @RequestBody AuthDTO authDTO) {
+        return ResponseEntity.ok(new APIResponse(200, "OK", authService.authenticate(authDTO)));
+    }
+}
