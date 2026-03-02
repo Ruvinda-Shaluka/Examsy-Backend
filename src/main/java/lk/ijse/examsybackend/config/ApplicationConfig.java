7be540114f2c7ec6f43e21af738bd269077f270b
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-02 20:38:57 +0530

    feat: configure security settings with JWT authentication and user details service

--- src/main/java/lk/ijse/examsybackend/config/ApplicationConfig.java
+++ src/main/java/lk/ijse/examsybackend/config/ApplicationConfig.java
@@ -1,4 +1,34 @@
 package lk.ijse.examsybackend.config;
 
+import lk.ijse.examsybackend.repository.UserAccountRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+    private final UserAccountRepo userAccountRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userAccountRepository.findByUsername(username)
+                .map(user -> new org.springframework.security.core.userdetails.User(
+                        user.getUsername(),
+                        user.getPasswordHash(), // Note: passwordHash instead of password
+                        List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+                )).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
