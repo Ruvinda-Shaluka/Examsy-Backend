c563b861dca9f9773156dd48e651d5fe08078ba7
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-02 20:43:36 +0530

    feat: add APIResponse, AuthDTO, and AuthResponseDTO classes for authentication handling

---
+++ src/main/java/lk/ijse/examsybackend/dto/AuthResponseDTO.java
@@ -0,0 +1,10 @@
+package lk.ijse.examsybackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String accessToken;
+}
