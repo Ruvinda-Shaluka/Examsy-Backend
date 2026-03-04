601538a2e1c2c937cd429200ac5bc68041e6d25d
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-04 15:57:08 +0530

    feat: add TeacherController, TeacherRepo with custom query, and TeacherService for teacher management

--- src/main/java/lk/ijse/examsybackend/repository/TeacherRepo.java
+++ src/main/java/lk/ijse/examsybackend/repository/TeacherRepo.java
@@ -2,6 +2,11 @@ package lk.ijse.examsybackend.repository;
 
 import lk.ijse.examsybackend.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
-public interface TeacherRepo extends JpaRepository<Teacher,Integer> {
+public interface TeacherRepo extends JpaRepository<Teacher, Integer> {
+    @Query("SELECT t FROM Teacher t WHERE t.userAccount.username = :username")
+    Optional<Teacher> findByUserAccountUsername(@Param("username") String username);
 }
---
+++ src/main/java/lk/ijse/examsybackend/service/TeacherService.java
@@ -0,0 +1,4 @@
+package lk.ijse.examsybackend.service;
+
+public class TeacherService {
+}
