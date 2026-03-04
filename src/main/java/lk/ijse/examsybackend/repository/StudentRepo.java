c882a0902c1228baaeddd00824d1d9c7955c3c86
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-04 10:23:10 +0530

    feat: add custom query method to StudentRepo for finding students by username

--- src/main/java/lk/ijse/examsybackend/repository/StudentRepo.java
+++ src/main/java/lk/ijse/examsybackend/repository/StudentRepo.java
@@ -2,6 +2,13 @@ package lk.ijse.examsybackend.repository;
 
 import lk.ijse.examsybackend.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface StudentRepo extends JpaRepository<Student,Integer> {
+
+    @Query("SELECT s FROM Student s WHERE s.userAccount.username = :username")
+    Optional<Student> findByUserAccountUsername(@Param("username") String username);
 }
