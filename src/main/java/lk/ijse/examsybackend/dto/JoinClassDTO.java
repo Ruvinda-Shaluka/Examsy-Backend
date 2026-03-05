7c5ba125578dc2f7d40853d80a46a45113a68636
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-05 22:07:22 +0530

    feat: add the JoinClassDTO to transfer invite link to enroll student

---
+++ src/main/java/lk/ijse/examsybackend/dto/JoinClassDTO.java
@@ -0,0 +1,18 @@
+package lk.ijse.examsybackend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JoinClassDTO {
+
+    @NotBlank(message = "Invite link is required.")
+    private String inviteLink;
+
+}
