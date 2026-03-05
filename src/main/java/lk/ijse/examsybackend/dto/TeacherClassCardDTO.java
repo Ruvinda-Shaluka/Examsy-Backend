50243d82cf353584c927e6cf8ee3ae9a69033ace
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-05 13:22:02 +0530

    feat: create TeacherClassCardDTO to mirror what TeacherClassCard.jsx needs to render

---
+++ src/main/java/lk/ijse/examsybackend/dto/TeacherClassCardDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.examsybackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TeacherClassCardDTO {
+    private Integer id;
+    private String title;
+    private String section;
+    private String bannerColor;
+}
