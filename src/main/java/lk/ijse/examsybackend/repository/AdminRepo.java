2352d5f3987ef1469b98fdfbd97a065776602f56
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-06 15:04:43 +0530

    feat: add AdminProfileService to handle admin profile related database handlers

--- src/main/java/lk/ijse/examsybackend/repository/AdminRepo.java
+++ src/main/java/lk/ijse/examsybackend/repository/AdminRepo.java
@@ -1,7 +1,9 @@
 package lk.ijse.examsybackend.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import lk.ijse.examsybackend.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface AdminRepo extends JpaRepository<Admin,Integer> {
+public interface AdminRepo extends JpaRepository<Admin, Integer> {
+    Optional<Admin> findByUserAccountUsername(String username);
 }
