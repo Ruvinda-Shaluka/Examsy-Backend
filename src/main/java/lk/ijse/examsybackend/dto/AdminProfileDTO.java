9d4667a1370184bbe012f3b83c716bad178b3011
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-06 14:53:05 +0530

    feat: add AdminProfileDTO transfer admin profile data

---
+++ src/main/java/lk/ijse/examsybackend/dto/AdminProfileDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.examsybackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AdminProfileDTO {
+    private Integer id;
+    private String fullName;
+    private String profilePictureUrl;
+    private String roleLevel;
+}
