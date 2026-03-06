df2fb198c2c57727ba5434755ff8cf8aed3a8a1d
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-06 16:44:07 +0530

    feat: add AdminProfileController to handle admin profile api end points

---
+++ src/main/java/lk/ijse/examsybackend/controller/AdminProfileController.java
@@ -0,0 +1,36 @@
+package lk.ijse.examsybackend.controller;
+
+import jakarta.validation.Valid;
+import lk.ijse.examsybackend.dto.AdminProfileDTO;
+import lk.ijse.examsybackend.dto.AdminProfileUpdateDTO;
+import lk.ijse.examsybackend.service.AdminProfileService;
+import lk.ijse.examsybackend.util.APIResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/admins/me")
+@RequiredArgsConstructor
+@Validated
+public class AdminProfileController {
+
+    private final AdminProfileService adminProfileService;
+
+    @GetMapping
+    public ResponseEntity<APIResponse<AdminProfileDTO>> getProfile(@AuthenticationPrincipal UserDetails user) {
+        return ResponseEntity.ok(new APIResponse<>(200, "Success", adminProfileService.getMyProfile(user.getUsername())));
+    }
+
+    @PutMapping
+    public ResponseEntity<APIResponse<AdminProfileDTO>> updateProfile(
+            @AuthenticationPrincipal UserDetails user,
+            @Valid @RequestBody AdminProfileUpdateDTO dto) {
+
+        AdminProfileDTO updatedProfile = adminProfileService.updateProfile(user.getUsername(), dto);
+        return ResponseEntity.ok(new APIResponse<>(200, "Profile updated successfully", updatedProfile));
+    }
+}
