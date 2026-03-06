1ece988743e46ed5e4ee67e0a1befafbea4e649b
Ruvinda24 <shalukaofficial24@gmail.com>
2026-03-06 13:33:57 +0530

    feat: add NotificationDTO to handle notification data transfers

---
+++ src/main/java/lk/ijse/examsybackend/dto/NotificationDTO.java
@@ -0,0 +1,19 @@
+package lk.ijse.examsybackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NotificationDTO {
+    private Integer id;
+    private String title;
+    private String message;
+    private Boolean isRead;
+    private LocalDateTime createdAt;
+}
